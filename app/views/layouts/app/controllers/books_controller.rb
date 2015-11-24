864ffdc24dd5219b3a5e7e2c6b774fa4ac839781
changahn1 <cyahn@sfu.ca>
2015-11-24 04:00:12 +0000

    added background image

---
+++ app/views/layouts/app/controllers/books_controller.rb
@@ -0,0 +1,64 @@
+class BooksController < ApplicationController
+    before_action :find_book, only: [:show, :edit, :update, :destroy]
+    before_action :authenticate_user!, only: [:new, :edit]
+    
+    def index 
+        if params[:department].blank?
+            @books = Book.all.order("created_at DESC")
+        else
+            @department_id = Department.find_by(name: params[:department]).id
+            @books = Book.where(:department_id => @department_id).order("created_at DESC")
+        end
+    end
+    
+    def show
+    end
+    
+    def new
+        @book = current_user.books.build
+        @departments = Department.all.map {|c| [c.name, c.id]}
+    end
+    
+    def create 
+        @book = current_user.books.build(book_params)
+        @book.department_id = params[:department_id]
+        @book.user_username = current_user.username
+        @book.user_email = current_user.email
+        
+        if @book.save
+            redirect_to root_path
+        else
+            render 'new'
+        end
+    end
+    
+    def edit
+        @departments = Department.all.map {|c| [c.name, c.id]}
+        
+    end
+    
+    def update
+        @book.department_id = params[:department_id]
+        if @book.update(book_params)
+            redirect_to book_path(@book)
+        else
+            render 'edit'
+        end
+    end
+    
+    def destroy
+        @book.destroy
+        redirect_to root_path
+    end
+    
+    private
+    
+    def book_params
+       params.require(:book).permit(:avatar, :title, :cname, :cnumber, :quality, :publisher, :campus, :price, :category_id) 
+    end
+    
+    def find_book
+        @book = Book.find(params[:id])
+    end
+    
+end
---
+++ app/views/layouts/app/models/book.rb
@@ -0,0 +1,8 @@
+class Book < ActiveRecord::Base
+    belongs_to :user
+    belongs_to :department
+    has_many :reviews
+    
+    has_attached_file :avatar, :styles => { :medium => "250x350>", :thumb => "325x475" }, :default_url => "/images/:style/missing.png"
+    validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
+end
